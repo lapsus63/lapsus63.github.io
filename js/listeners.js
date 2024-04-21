b346c34054c8c0f21ff012f3380521537939bde5
Olivier Chorier <lapsus63@gmail.com>
2024-04-21 16:05:53 +0200

    Update devoxx

---
+++ js/listeners.js
@@ -0,0 +1,15 @@
+window.onload = function(event) {
+      cont = document.getElementsByClassname("uk-body")[0];
+      //if (screen.width < 850) {
+      if (navigator.userAgentData.mobile) {
+        cont.style.width = '100%';
+        cont.style.marginLeft = '0px';
+        cont.style.marginRight = '0px';
+        cont.style.fontSize = '42px';
+      } else {
+        cont.style.width = '75%';
+        cont.style.marginLeft = 'auto';
+        cont.style.marginRight = 'auto';
+        cont.style.fontSize = '16px';
+      }
+    };
